62cf00c172c1259d77ae26f623547b0fde64d337
Kelvin-Ben <kevoben14@gmail.com>
2024-03-27 23:10:57 +0300

    Create eventlistener for different modules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import _ from "lodash";
 import "./style.css";
+import { createGame } from "../module/addGame";
+import { addScore } from "../module/addScore";
+import { displayScores } from "../module/getScores";
 
+// console.log(g)
+const scoreForm = document.querySelector(".score-form");
+// console.log(scoreForm);
+const refreshButton = document.querySelector(".btn-refresh");
+
+const gameForm = document.querySelector("#new-game-form");
+gameForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const gameName = document.querySelector("#game-name").value.trim();
+  if (gameName) {
+    try {
+      const gameId = await createGame(gameName);
+      alert(`Game created successfully with ID: ${gameId}`);
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+});
+
+scoreForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  // const gameId = "8KxQN8JEsLJHjYXAHA4Z ";
+  const gameId = "VJ4h0YHaRn8XrWzKuzF8";
+  const userName = document.querySelector(".input-name").value.trim();
+  const userScore = document.querySelector(".input-score").value.trim();
+  if (userName && userScore) {
+    try {
+      await addScore(gameId, userName, userScore);
+      alert("Score added successfully.");
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+});
+refreshButton.addEventListener("click", async () => {
+  displayScores();
+});
